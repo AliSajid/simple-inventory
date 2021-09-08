c24846cf689507d271416c6d3eb8cae41323bcbb
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2021-09-07 22:00:22 -0400

    Trivial test to ensure tests are working

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,14 @@
-from app import __version__
+from src.app import __version__, app
+import json
 
+app.testing = True
+client = app.test_client()
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_root():
+    response = client.get("/")
+    data = json.loads(response.data)
+    assert data['status_code'] == 200
+    assert data['response'] == "Hello"
