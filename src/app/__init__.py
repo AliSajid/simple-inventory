e3771c07aab7c2ea4abf5309ae05eff4e126071d
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2021-09-07 21:51:13 -0400

    Basic app for testing

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,1 +1,12 @@
 __version__ = '0.1.0'
+
+
+from flask import Flask
+import json
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    response =  {"response": "Hello", "status_code": 200}
+    return json.dumps(response)
