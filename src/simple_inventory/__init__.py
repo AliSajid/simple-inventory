661e5c45a72f58f65f3282db94e9b541887cec6c
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2021-09-08 23:32:40 -0400

    Added an application factory with passing tests

--- src/simple_inventory/__init__.py
+++ src/simple_inventory/__init__.py
@@ -1,12 +1,44 @@
+"""
+The simple-inventory module implements a flask application
+that can work as a simple inventory app.
+"""
+
 __version__ = '0.1.0'
 
 
-from flask import Flask
 import json
+import os
+from flask import Flask
 
-app = Flask(__name__)
 
-@app.route("/")
-def index():
-    response =  {"response": "Hello", "status_code": 200}
-    return json.dumps(response)
+def create_app(test_config=None):
+    """
+    This is a factory method that will create and return our application
+
+    """
+    app = Flask(__name__, instance_relative_config=True)
+
+    app.config.from_mapping(
+        SECRET_KEY="dev",
+        PONY={
+            'provider': 'postgres',
+            'username': 'simpinv',
+            'password': os.environ.get("ENV_POSTGRES_PASSWORD") or "simpinv",
+            'host': os.environ.get("ENV_POSTTGRES_HOST") or "127.0.0.1",
+            'database': 'simpinv'
+        },
+    )
+
+    if test_config is None:
+        app.config.from_pyfile("config.py", silent = True)
+    else:
+        app.config.from_mapping(test_config)
+
+
+    @app.route("/")
+    def index():
+        resp = {"status_code": 200,
+                "response": "Hello"}
+        return json.dumps(resp)
+
+    return app
--- tests/test_simple_inventory.py
+++ tests/test_simple_inventory.py
@@ -1,15 +1,18 @@
-from simple_inventory import __version__, app
+from simple_inventory import __version__, create_app
 import pytest
 import json
 
+app = create_app()
 app.testing = True
 client = app.test_client()
 
+
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == "0.1.0"
+
 
 def test_root():
     response = client.get("/")
     data = json.loads(response.data)
-    assert data['status_code'] == 200
-    assert data['response'] == "Hello"
+    assert data["status_code"] == 200
+    assert data["response"] == "Hello"
