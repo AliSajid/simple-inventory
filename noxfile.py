47af2216de4d59b1352447b4547ea5c394152668
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2021-09-08 23:32:16 -0400

    Added more nox sessions and cleaned up the pyproject files

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,38 @@
+"""
+Nox configuration file
+"""
+
 import nox
+
+nox.options.sessions = "lint", "tests"
+locations = "src", "tests", "noxfile.py"
+
 
 @nox.session(python=["3.7", "3.8", "3.9"])
 def tests(session):
+    """
+    Nox function for running tests.
+    """
+    args = session.posargs or ["--cov"]
     session.run("poetry", "install", external=True)
-    session.run("pytest", "--cov")
+    session.run("pytest", *args)
+
+
+@nox.session(python=["3.7", "3.8", "3.9"])
+def lint(session):
+    """
+    Nox function for linting.
+    """
+    args = session.posargs or locations
+    session.install("flake8")
+    session.run("flake8", *args)
+
+
+@nox.session(python=["3.7", "3.8", "3.9"])
+def black(session):
+    """
+    Nox function for running the black formatter.
+    """
+    args = session.posargs or locations
+    session.install("black")
+    session.run("black", *args)
